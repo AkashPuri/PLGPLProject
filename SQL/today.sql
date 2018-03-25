2ba6d8f201a001275b4897bb7c28bfe72334fe96
Akash-Sachin <sheireshpuri@gmail>
2018-03-25 13:30:45 +0530

    Allocate subject changes

---
+++ SQL/today.sql
@@ -0,0 +1,118 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: facultydatabase
+-- ------------------------------------------------------
+-- Server version	5.5.3-m3-community
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `faculty`
+--
+
+DROP TABLE IF EXISTS `faculty`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `faculty` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `designation` varchar(45) NOT NULL,
+  `qualification` varchar(45) NOT NULL,
+  `institute` varchar(100) NOT NULL,
+  `contact` varchar(45) NOT NULL,
+  `emailId` varchar(45) DEFAULT NULL,
+  `username` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username_UNIQUE` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `faculty`
+--
+
+LOCK TABLES `faculty` WRITE;
+/*!40000 ALTER TABLE `faculty` DISABLE KEYS */;
+INSERT INTO `faculty` VALUES (1,'Akash','Head Of Department','Ph. D.','IIT -Bombay','9096407698','faculty1@gmail.com','user1'),(3,'Shubham','Head Of Department','Ph. D.','IIT-Madras','9096407698','faculty2@gmail.com','user2'),(4,'Sachin Jadhav','Faculty','M. Tech','VIT Pune','1234567890','sachinjadha@gmail.com','sachin');
+/*!40000 ALTER TABLE `faculty` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `subjectallocation`
+--
+
+DROP TABLE IF EXISTS `subjectallocation`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `subjectallocation` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `subjectname` varchar(45) NOT NULL,
+  `facultyname` varchar(45) NOT NULL,
+  `time` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `subjectallocation`
+--
+
+LOCK TABLES `subjectallocation` WRITE;
+/*!40000 ALTER TABLE `subjectallocation` DISABLE KEYS */;
+INSERT INTO `subjectallocation` VALUES (1,'Subject 1','Akash','time 1'),(2,'Subject 2','Shubham','time 2'),(3,'Subject 3','Shubham','time 3'),(4,'Subject 1','Shubham','time 4'),(5,'Subject 4','Akash','time 4'),(6,'Subject 4','Sachin Jadhav','time 1');
+/*!40000 ALTER TABLE `subjectallocation` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usertable`
+--
+
+DROP TABLE IF EXISTS `usertable`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `usertable` (
+  `username` varchar(50) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `role` varchar(45) NOT NULL,
+  `facultyName` varchar(200) NOT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usertable`
+--
+
+LOCK TABLES `usertable` WRITE;
+/*!40000 ALTER TABLE `usertable` DISABLE KEYS */;
+INSERT INTO `usertable` VALUES ('faculty1','faculty1','faculty1',''),('njkwd','jdfdwnkjfdjfwjkfw','faculty',''),('sachin','sachin','faculty','Sachin Jadhav'),('user1','user1','admin','Akash'),('user2','user2','faculty','Shubham'),('user3','user3','student','');
+/*!40000 ALTER TABLE `usertable` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'facultydatabase'
+--
+
+--
+-- Dumping routines for database 'facultydatabase'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-03-25 13:24:31
